b3247478903b6c34fa7e0f4480d90b08a300d39a
SarvaPulla <sarvapulla@gmail.com>
2018-05-01 19:27:30 +0000

    Debug

--- build/lib/tethysapp/rheas_viewer/config.py
+++ build/lib/tethysapp/rheas_viewer/config.py
@@ -1,11 +1,11 @@
 connection = {'host': '52.2.219.167',
               'user': 'rheas',
-              'password': 'pass',
+              'password': 'rhe@s2016',
               'dbname': 'rheas'}
 
 geoserver = {'rest_url':'https://tethys.servirglobal.net/geoserver/rest',
              'wms_url':'https://tethys.servirglobal.net/geoserver/wms',
              'wfs_url':'https://tethys.servirglobal.net/geoserver/wfs',
              'user':'admin',
-             'password':'pass',
+             'password':'Tethys2018',
              'workspace':'rheas'}
